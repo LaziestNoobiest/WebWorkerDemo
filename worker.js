d255e6123da7685d195c7d8a79b581daaf5795ec
LaziestNoobiest <ucmcirclek.tech@gmail.com>
2017-02-10 23:23:31 -0800

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,9 @@
+var rad	 = Math.PI/180;
+
+function oscillate()
+{
+	rad = (rad >= Math.PI*178/180)? (Math.PI/180) : (rad + Math.PI/180);
+	postMessage(rad); // post rad back to html page
+}
+
+var t = setInterval(oscillate, 100);
